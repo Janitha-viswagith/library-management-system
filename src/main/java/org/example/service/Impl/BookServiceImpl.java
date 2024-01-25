768311103514d2e2d205be8963ed174b485574b3
Janitha-viswagith <vishwagithjanith@gmail.com>
2024-01-25 14:15:11 +0530

    add yml file

--- src/main/java/org/example/service/Impl/BookServiceImpl.java
+++ src/main/java/org/example/service/Impl/BookServiceImpl.java
@@ -2,7 +2,9 @@ package org.example.service.Impl;
 
 import org.example.dto.Book;
 import org.example.service.BookService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
     @Override
     public void addBook(Book book) {
